8e73341797ca54796bb548c20ddcac9d78e8d87e
FehMacedo10 <fmacedoribeiro@outlook.com>
2025-01-28 15:18:18 -0300

    :sparkles: feat: Implementação da Model e o CRUD Categorias

---
+++ src/main/java/com/generation/deliverymandapramim/model/Categorias.java
@@ -0,0 +1,63 @@
+package com.generation.deliverymandapramim.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categorias {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O campo Nome é obrigatório para a digitação")
+	@Size(min = 5, max = 100, message = "O campo Nome deve conter no mínimo 05 e no máximo 100 caracteres")
+	private String nome;
+	
+	@NotBlank(message = "O campo Descrição é obrigatório para a digitação")
+	@Size(min = 5, max = 255, message = "O campo Descrição deve conter no mínimo 05 e no máximo 255 caracteres")
+	private String descricao;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categorias", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categorias")
+	private List<Produtos> produto;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+}
